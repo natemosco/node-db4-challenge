c0a38d11666f9fc2258cde3a292e08c7cd4f4c30
Nathaniel Mosco <natemosco@gmail.com>
2019-12-14 12:05:51 -0500

    added table model functions to the rescipe migrations file

--- data/migrations/20191214111742_recipes.js
+++ data/migrations/20191214111742_recipes.js
@@ -2,18 +2,40 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("recipes", table => {
       table.increments();
+      table
+        .string("Name", 255)
+        .notNullable()
+        .unique();
+      table.string("instructions", 1000).notNullable();
     })
     .createTable("ingredients", table => {
       table.increments();
+      table.string("Name", 255).notNullable();
     })
-    .createTable("recipe_ingredients", table => {
+    .createTable("Recipe_Ingredient", table => {
       table.increments();
+      table.string("Quantity", 255).notNullable();
+      table
+        .integer("Ingredients_Id")
+        .unsigned()
+        .references("id")
+        .inTable("Ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      table
+        .integer("Recipie_Id")
+        .unsigned()
+        .references("id")
+        .inTable("Recipie")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists("recipe_ingredients")
-    .dropTableIfExists("ingredients")
-    .dropTableIfExists("recipes");
+    .dropTableIfExists("Recipe_Ingredient")
+    .dropTableIfExists("Ingredients")
+    .dropTableIfExists("Recipes");
 };
